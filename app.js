b760a2b51443c900025910e37db2ac5e18135b43
RinkM <mikerinkert@gmail.com>
2022-07-05 19:06:59 -0600

    working app.

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ let boxArray = [];
 let allBoxes = [];
 let box;
 const slider = document.querySelector('.slider');
-const sliderOutput = document.querySelector('.gridSize');
-let size = slider.value;
+const gridSize = document.querySelector('.gridSize');
+let size =5;
 let color = "black";
 let height;
 let width;
@@ -27,8 +27,15 @@ const redButton = document.querySelector('.red');
 const greenButton = document.querySelector('.green');
 const blueButton = document.querySelector('.blue');
 const recreateGrid = document.querySelector('.recreateGrid');
+const colorPicker = document.querySelector('.colorPicker');
+
+
+
 
 function buildGrid(size){
+    colorPicker.style.background = color;
+    workspaceHeight = workspace.clientHeight;
+    workspaceWidth = workspace.clientWidth;
     height = workspaceHeight / size;
     width = workspaceWidth / size;
 
@@ -76,23 +83,30 @@ recreateGrid.addEventListener('click', ()=>{
 
 
 
-clearButton.addEventListener('click', ()=>{
-    clearColors()});
+// clearButton.addEventListener('click', ()=>{
+//     clearColors()});
 
-blackButton.addEventListener('click', ()=>{
-    color = "black"});
+// blackButton.addEventListener('click', ()=>{
+    
+//     color = "black";
+//     colorPicker.style.background = color;
+// });
 
-whiteButton.addEventListener('click', ()=>{
-    color = "white"});
+// whiteButton.addEventListener('click', ()=>{
+//     color = "white";
+//     colorPicker.style.background = color;});
 
-redButton.addEventListener('click', ()=>{
-    color = "red"});
+// redButton.addEventListener('click', ()=>{
+//     color = "red";
+//     colorPicker.style.background = color;});
 
-greenButton.addEventListener('click', ()=>{
-    color = "green"});
+// greenButton.addEventListener('click', ()=>{
+//     color = "green";
+//     colorPicker.style.background = color;});
 
-blueButton.addEventListener('click', ()=>{
-    color = "blue"});
+// blueButton.addEventListener('click', ()=>{
+//     color = "blue";
+//     colorPicker.style.background = color;});
 
 
 
@@ -112,56 +126,121 @@ console.log(workspace.childNodes)
 
 
 
+// the knobs!   use offsetX to find the x value of cursor   use a percentage to 
+// x = 0 then rotation  = -90
+// x = mid then rotation = 0 
+// x = max then rotation = +90
 
 
 
-
-
-
-
-
-
-sliderOutput.innerHTML = slider.value;
-
-slider.oninput = function() {
-    sliderOutput.innerHTML = this.value;
-    size = this.value;
-}
-
-
-const knob = document.querySelector('.knob');
+const knobList = document.querySelectorAll('.knob');
 
 function calculateDegree(e){
-    const x1 = window.innerWidth/2;
-    const y1 = window.innerHeight/2;
     const x2 = e.clientX;
-    const y2 = e.clientY;
-    
+    const x3left = knobList[0].offsetLeft
+    const x3right = knobList[1].offsetLeft
 
-    const deltaX = x1 - x2;
-    const deltaY = y1 - y2;
+    // console.log(window.innerWidth)
+    // console.log(x2) 
     // console.log(x3)
-    // console.log("x2:" + x2)
-    // console.log("y1,y2:" + [y1,y2])
-    const rad = Math.atan2(deltaY,deltaX);
-    let deg = rad *(180/Math.PI);
-    // console.log(deg);
-    return deg;
+
+    dialValueLeft =  (((x2 - x3left)/64)*180)-90;
+    dialValueRight = (((x2 - x3right)/64)*180)-90;
+    // console.log("deltaX: " + (deltaX))
+    
+    console.log("left: " + dialValueLeft)
+    console.log("right: " + dialValueRight)
+    return [dialValueLeft, dialValueRight];
 
 }
+// knobList.map(knob => {
+//     knob.addEventListener("mousedown",function() {
+//         knob.addEventListener("mousemove", rotate);
+//             function rotate(e){
+//                 // console.log(e)
+//                 const result = Math.floor(calculateDegree(e));
+//                 knob.style.transform = `rotate(${result}deg)`;
+//                 // console.log(knob.style.transform)
+//             }
+//             knob.addEventListener("mouseup", function(){
+//                 knob.removeEventListener("mousemove", rotate);
+//             });
+//     });
 
-knob.addEventListener("mousedown",function() {
-    knob.addEventListener("mousemove", rotate);
+// });
+
+let dialValueLeft = 0;
+let dialValueRight = 0;
+
+function updateColor (){
+    console.log(dialValueRight+135)
+    if (dialValueRight + 135 <= 68) {
+        color = "white";
+        colorPicker.style.background = color;
+    } else if (dialValueRight + 135 >= 69 && dialValueRight + 135 <= 135){
+        color = "black";
+        colorPicker.style.background = color;
+    } else if (dialValueRight + 135 >= 136 && dialValueRight + 135 <= 203){
+        color = "green";
+        colorPicker.style.background = color;
+    } else if (dialValueRight + 135 >= 204){
+        color = "blue";
+        colorPicker.style.background = color;
+    } else {
+        console.log ("right value dial if statementes broke.")
+    }
+}
+
+function updateSize(){
+console.log(dialValueLeft+135)
+   if (dialValueLeft + 135 <= 68) {
+    size = 2;    
+    gridSize.textContent = "2"
+
+    } else if (dialValueLeft + 135 >= 69 && dialValueLeft + 135 <= 135){
+        size = 5;
+        gridSize.textContent = "5";
+    } else if (dialValueLeft + 135 >= 136 && dialValueLeft + 135 <= 203){
+        size = 25;
+        gridSize.textContent = "25"
+    } else if (dialValueLeft + 135 >= 204){
+        size = 50;
+        gridSize.textContent = "50"
+    } else {
+        console.log ("right value dial if statementes broke.")
+    }
+}
+
+
+knobList[0].addEventListener("mousedown",function() {
+    knobList[0].addEventListener("mousemove", rotate);
         function rotate(e){
+                
+
             // console.log(e)
-            const result = Math.floor(calculateDegree(e));
-            knob.style.transform = `rotate(${result}deg)`;
-            // console.log(knob.style.transform)
+            const result = Math.floor(calculateDegree(e)[0]);
+            knobList[0].style.transform = `rotate(${result}deg)`;
+            updateSize()
+            // console.log(knobList[0].style.transform)
         }
-        knob.addEventListener("mouseup", function(){
-            knob.removeEventListener("mousemove", rotate);
+        knobList[0].addEventListener("mouseup", function(){
+            knobList[0].removeEventListener("mousemove", rotate);
         });
 });
+
+knobList[1].addEventListener("mousedown",function() {
+    knobList[1].addEventListener("mousemove", rotate);
+        function rotate(e){
+            // console.log(e)
+            const result = Math.floor(calculateDegree(e)[1]) ;
+            knobList[1].style.transform = `rotate(${result}deg)`;
+            updateColor()
+            // console.log(knobList[1].style.transform)
+        }
+        knobList[1].addEventListener("mouseup", function(){
+            knobList[1].removeEventListener("mousemove", rotate);
+        });
+    });
 
 // let screenLog = document.querySelector('#screen-log');
 // document.addEventListener('mousemove', logKey);
